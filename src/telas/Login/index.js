5180a58ba9a89994d68f7326c897976b139e2630
Daniel <danielvitorcg@gmail.com>
2022-10-03 14:05:13 -0300

    feat: login with user context

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -5,22 +5,43 @@ import {
   TextInput,
   TouchableOpacity,
   StatusBar,
+  Alert,
 } from 'react-native'
 import { estilos } from './estilos'
 import { useContext } from 'react'
 import { ThemeContext } from '../../contexts/themeContext'
+import { UserContext } from '../../contexts/userContext'
+import Configuracao from '../Configuracao'
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
 
   const { themeSelected } = useContext(ThemeContext)
-
   const estilo = estilos(themeSelected)
+
+  const { login } = useContext(UserContext)
+
+  function logIn() {
+    const res = login(email, senha)
+    if (res === 'ok') {
+      navigation.navigate('Principal')
+    } else {
+      Alert.alert(res)
+    }
+  }
 
   return (
     <View style={estilo.container}>
       <StatusBar />
+      <TouchableOpacity style={estilo.iconArea}>
+        <Configuracao
+          name='settings'
+          size={30}
+          color='#fff'
+          style={estilo.icon}
+        />
+      </TouchableOpacity>
       <Text style={estilo.titulo}>Login</Text>
 
       <View style={estilo.inputArea}>
@@ -39,13 +60,11 @@ export default function Login({ navigation }) {
           autoCapitalize='none'
           value={senha}
           onChangeText={setSenha}
+          secureTextEntry={true}
         />
       </View>
 
-      <TouchableOpacity
-        style={estilo.botao}
-        onPress={() => navigation.navigate('Principal')}
-      >
+      <TouchableOpacity style={estilo.botao} onPress={() => logIn()}>
         <Text style={estilo.botaoTexto}>Entrar</Text>
       </TouchableOpacity>
     </View>
